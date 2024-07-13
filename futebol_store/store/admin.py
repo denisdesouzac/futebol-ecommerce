49687f0919f4cf37217348da6d0d43a91881475a
Ronald <ronalddesouzagaldino@gmail.com>
2024-07-13 16:22:21 -0300

    Adicionando models do cliente no ambiente administrativo

--- futebol_store/store/admin.py
+++ futebol_store/store/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Client
+from .models import Order
+from .models import OrderItem
+from .models import Payment
+
+
+admin.site.register(Client)
+admin.site.register(Order)
+admin.site.register(OrderItem)
+admin.site.register(Payment)
